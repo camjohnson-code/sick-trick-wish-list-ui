d01cc62619c96055c1f52ac18301a2a1f3682c3b
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-20 09:46:11 -0700

    Display tricks on page load

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,38 @@
 import './App.css';
-import React from 'react';
-
-
+import React, { useEffect, useState } from 'react';
+import Card from '../Card/Card';
+import { getTricks } from '../../ApiCalls';
 
 function App() {
+  const [tricks, setTricks] = useState(null);
+
+  useEffect(() => {
+    getTricks('http://localhost:3001/api/v1/tricks').then((tricks) =>
+      setTricks(tricks)
+    );
+  }, []);
+
+  const cardElements = tricks
+    ? tricks.map((trick) => {
+        return (
+          <Card
+            stance={trick.stance}
+            name={trick.name}
+            obstacle={trick.obstacle}
+            tutorial={trick.tutorial}
+            id={trick.id}
+            key={trick.id}
+          />
+        );
+      })
+    : null;
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Sick Trick Wish List</h1>
+      <section className='trick-section'>{cardElements}</section>
     </div>
   );
 }
 
-export default App; 
+export default App;
