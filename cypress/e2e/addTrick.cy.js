048a1e2f7f3a1152d90fd51891307da9f51ab652
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-20 11:51:00 -0700

    Create test for adding a new trick

--- cypress/e2e/addTrick.cy.js
+++ cypress/e2e/addTrick.cy.js
@@ -1,5 +1,16 @@
 describe('empty spec', () => {
   beforeEach(() => {
+    cy.intercept('POST', 'http://localhost:3001/api/v1/tricks', {
+      statusCode:  200,
+      body: {
+        stance: 'Regular',
+        name: 'New York Nut Buster',
+        obstacle: 'Stairs',
+        tutorial: 'https://www.youtube.com/watch?v=O5x-y-Y8Gzs',
+        id:  12345
+      }
+    })
+
     cy.intercept('http://localhost:3001/api/v1/tricks', {
       statusCode: 200,
       fixture: '/example.json',
@@ -9,9 +20,14 @@ describe('empty spec', () => {
 
   it('adds a trick to the list', () => {
     cy.get('form select').first().select('Regular')
-    .get('form input').first().click().type('New York Nut Buster')
-    .get('form select').last().select('Stairs')
-    .get('form input').last().click().type('https://www.youtube.com/watch?v=O5x-y-Y8Gzs')
-    .get('form button').click()
-    })
+      .get('form input').first().click().type('New York Nut Buster')
+      .get('form select').last().select('Stairs')
+      .get('form input').last().click().type('https://www.youtube.com/watch?v=O5x-y-Y8Gzs')
+    cy.get('button').click()
+    cy.intercept('http://localhost:3001/api/v1/tricks', {
+      statusCode: 200,
+      fixture: '/addedTrick.json',
+    });
+    cy.reload()
+  })
 })
