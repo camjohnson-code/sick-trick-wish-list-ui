c942526f2cdaa1b5001bc3d69919c80e61532c87
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-20 09:51:32 -0700

    Add error handling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import Card from '../Card/Card';
+import Form from '../Form/Form';
 import { getTricks } from '../../ApiCalls';
 
 function App() {
   const [tricks, setTricks] = useState(null);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    getTricks('http://localhost:3001/api/v1/tricks').then((tricks) =>
-      setTricks(tricks)
-    );
+    getTricks('http://localhost:3001/api/v1/tricks')
+      .then((tricks) => setTricks(tricks))
+      .catch((error) => setError('There was an error. Please try again.'));
   }, []);
 
   const cardElements = tricks
@@ -30,7 +32,10 @@ function App() {
   return (
     <div className='App'>
       <h1>Sick Trick Wish List</h1>
-      <section className='trick-section'>{cardElements}</section>
+      <Form />
+      <section className='trick-section'>
+        {tricks ? cardElements : <p>{error}</p>}
+      </section>
     </div>
   );
 }
