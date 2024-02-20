2fb212bfc2e16bf9f4e8523ec22ae5bcc3bcd069
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-20 10:33:23 -0700

    Add POST to server

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,3 +1,13 @@
 export const getTricks = (url) => {
   return fetch(url).then((response) => response.json());
 };
+
+export const addTrick = (url, data) => {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+};
